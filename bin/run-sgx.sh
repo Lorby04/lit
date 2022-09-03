7fe6ed690c7e82da969728f83c4ca23e18339cea
Lorby Bi <lorby.bi@gmail.com>
2022-09-03 14:17:55 -0500

    c

--- bin/run-sgx.sh
+++ bin/run-sgx.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 10
 ~/record/top.sh cpu-ring.log &
 disown
 gramine-sgx lit ring -n 1 -s 4 -t 10M | tee sgx-ring.log &
@@ -8,7 +9,8 @@ sleep 5
 
 killall -9 top
 ~/record/top.sh cpu-list.log &
+disown
 gramine-sgx lit list -n 1 -s 4 -t 10M | tee sgx-list.log &
-sleep 100
+sleep 5
 ./lit list -n 1 -s 4 -t 10M | tee native.log-list &
 sleep 5
